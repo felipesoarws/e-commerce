98702296a8353ba88e665ec631aea222ee95067a
felp <ppfelp@gmail.com>
2022-07-20 14:41:52 -0300

    new commit

--- src/components/cards.js
+++ src/components/cards.js
@@ -5,33 +5,51 @@ import AirMaxOffWhite from "../images/airmaxoffwhite.png";
 import AirMax90 from "../images/airmax90.png";
 import AdidasNmd from "../images/adidasnmd.png";
 import Balenciaga from "../images/balenciaga.png";
+import { useEffect, useState } from "react";
 
-import Card from "./card";
+function addToCart(el) {
+  let card = el.nativeEvent.path[2];
+  let bttn = card.querySelector("button");
+  bttn.innerHTML = "done!";
+
+  setTimeout(function () {
+    bttn.innerHTML = "add to cart";
+  }, 2000);
+}
 
 function Cards() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://raw.githubusercontent.com/felipesoarws/e-commerce/main/src/components/shoes.json"
+    )
+      .then((response) => response.json())
+      .then(setData);
+  }, []);
+
+  if (!data || !data.length) return null;
   return (
     <article className="cards">
-      <Card img={AirMax} price="99$" title="Nike Air Max 95 " class="blue" />
-      <Card
-        img={AirJordan}
-        price="120$"
-        title="Nike Air Jordan 1 "
-        class="red"
-      />
-      <Card
-        img={AirMaxOffWhite}
-        price="130$"
-        title="Nike Air Max Off-White"
-        class="bege"
-      />
-      <Card img={Balenciaga} price="150$" title="Balenciaga" class="black" />
-      <Card img={AdidasNmd} price="105$" title="Adidas NMD R1" class="gray" />
-      <Card
-        img={AirMax90}
-        price="125$"
-        title="Nike Air Max 90 White"
-        class="white"
-      />
+      {data.map((shoe) => {
+        const { id, color, price, name, image } = shoe;
+        return (
+          <section className={color} key={id}>
+            <div className="card">
+              <div className="f-line">
+                <div className="price">
+                  <h2>{price}</h2>
+                  <small>{name}</small>
+                </div>
+              </div>
+              <div>
+                <img src={image} alt="shoe" className="shoe" />
+                <button onClick={addToCart}>add to cart</button>
+              </div>
+            </div>
+          </section>
+        );
+      })}
     </article>
   );
 }
